e74b7aa19f8a287b41ad79fd4d1e807ef4c8dc9a
psyGamer <psygamer21@gmail.com>
2024-10-09 17:19:15 +0200

    Add profiler zones

---
+++ Tracy.NET/Native.cs
@@ -0,0 +1,85 @@
+using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace Tracy;
+
+/// Direct bindings for the native C functions
+internal static unsafe partial class Native
+{
+    private const string LibraryName = "TracyClient";
+
+    #region Functions
+
+    [LibraryImport(LibraryName, EntryPoint = "___tracy_emit_zone_begin")]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    public static partial TracyCZoneContext TracyEmitZoneBegin(TracySourceLocationData* srcloc, int active);
+
+    [LibraryImport(LibraryName, EntryPoint = "___tracy_emit_zone_end")]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    public static partial void TracyEmitZoneEnd(TracyCZoneContext ctx);
+
+    [LibraryImport(LibraryName, EntryPoint = "___tracy_emit_zone_text")]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    public static partial void TracyEmitZoneText(TracyCZoneContext ctx, CString txt, ulong size);
+
+    [LibraryImport(LibraryName, EntryPoint = "___tracy_emit_zone_name")]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    public static partial void TracyEmitZoneName(TracyCZoneContext ctx, CString txt, ulong size);
+
+    [LibraryImport(LibraryName, EntryPoint = "___tracy_emit_zone_color")]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    public static partial void TracyEmitZoneColor(TracyCZoneContext ctx, uint color);
+
+    [LibraryImport(LibraryName, EntryPoint = "___tracy_emit_zone_value")]
+    [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+    public static partial void TracyEmitZoneValue(TracyCZoneContext ctx, ulong value);
+
+    #endregion
+    #region Types
+
+    /// Wrapper around C strings
+    [StructLayout(LayoutKind.Sequential)]
+    public readonly struct CString(string? str) : IEquatable<CString>, IDisposable
+    {
+        private readonly IntPtr data = Marshal.StringToHGlobalAnsi(str);
+
+        /// Frees the allocated string
+        public void Dispose() => Marshal.FreeHGlobal(data);
+
+        /// Compares both strings for the same content
+        public bool Equals(CString other)
+        {
+            char* a = (char*)this.data, b = (char*)other.data;
+            while (*a != 0 || *b != 0)
+            {
+                if (*a != *b) return false;
+                a++;
+                b++;
+            }
+            return *a == *b;
+        }
+
+        public override bool Equals(object? obj) => obj is CString other && Equals(other);
+        public override int GetHashCode() => data.GetHashCode();
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct TracySourceLocationData
+    {
+        public CString name;
+        public CString function;
+        public CString file;
+        public uint line;
+        public uint color;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct TracyCZoneContext
+    {
+        public uint id;
+        public int active;
+    }
+
+    #endregion
+}
--- Tracy.NET/Tracy.cs
+++ Tracy.NET/Tracy.cs
@@ -1,6 +1,79 @@
-﻿namespace Tracy;
+﻿using System;
+using System.Runtime.CompilerServices;
+using static Tracy.Native;
 
-public class Tracy
+namespace Tracy;
+
+public unsafe class Tracy
 {
+    #region Zones
 
+    /// Represents a profiling zone
+    public readonly struct ZoneContext : IDisposable
+    {
+        private readonly TracyCZoneContext context;
+
+        internal ZoneContext(TracyCZoneContext context) {
+            this.context = context;
+        }
+
+        /// Whether the zone is currently sent to the profiler
+        public bool Active => context.active != 0;
+
+        /// Additional text to display along with the zone information
+        public string Text {
+            set {
+                using var textStr = new CString(value);
+                TracyEmitZoneText(context, textStr, (ulong)value.Length);
+            }
+        }
+
+        /// Additional numeric value to display along with the zone information
+        public ulong Value {
+            set => TracyEmitZoneValue(context, value);
+        }
+
+        /// Dynamically change the name of the zone
+        public string Name {
+            set {
+                using var nameStr = new CString(value);
+                TracyEmitZoneName(context, nameStr, (ulong)value.Length);
+            }
+        }
+
+        /// Dynamically change the color of the zone
+        public uint Color {
+            set => TracyEmitZoneColor(context, value);
+        }
+
+        public void Dispose() => TracyEmitZoneEnd(context);
+    }
+
+    /// Creates a new profiling zone until the end of the current scope
+    public static ZoneContext Zone(
+        string? name = null,
+        uint color = 0x000000,
+        bool active = true,
+        [CallerMemberName] string? function = null,
+        [CallerFilePath] string? file = null,
+        [CallerLineNumber] int line = 0)
+    {
+        using var nameStr = new CString(name);
+        using var functionStr = new CString(function);
+        using var fileStr = new CString(file);
+
+        var sourceLocation = new TracySourceLocationData
+        {
+            name = nameStr,
+            function = functionStr,
+            file = fileStr,
+            line = (uint)line,
+            color = color,
+        };
+        var context = TracyEmitZoneBegin(&sourceLocation, active ? 1 : 0);
+
+        return new ZoneContext(context);
+    }
+
+    #endregion
 }
