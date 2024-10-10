510c33422568d3fe45dbe3ad1f2767e00b7fc400
psyGamer <psygamer21@gmail.com>
2024-10-10 18:21:16 +0200

    Set up a MSBuild task after the project has been compiled

---
+++ Tracy.NET.MSBuild/InstrumentMethodsTask.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using Task = Microsoft.Build.Utilities.Task;
+
+namespace TracyNET.MSBuild;
+
+public class InstrumentMethodsTask : Task
+{
+    [Required]
+    public string AssemblyPath { get; set; } = null!;
+
+    public override bool Execute()
+    {
+        Log.LogWarning($"Got assembly path: {AssemblyPath}");
+        return true;
+    }
+}
